13de99baedce9aa1764cf3314e9c546c3c433f1f
Paul Zhang <3114938951@qq.com>
2019-09-06 18:23:01 +0800

    finished mod15 exercise1

--- Assignment5/mod15/exercise1/Person.java
+++ Assignment5/mod15/exercise1/Person.java
@@ -5,42 +5,49 @@ import java.util.List;
 import java.util.ArrayList;
 
 /***
-**** Step #2: declare that the Family class is serializable.
-***/
-public class Person implements Serializable{
-  // Attributes
-  private String firstName;
-  private String lastName;
-  /***
-  **** Step #3: declare that the numOfSiblings attribute is not
-  **** to be serialized.
-  ***/
-  private transient int numOfSiblings;
-  // Associations
-  private Family family = null;
-
-  // Constructor
-  public Person(Family fam, String f, String l) {
-    firstName = f;
-    lastName = l;
-    family = fam;
-    if ( family != null ) {
-      family.addChild(this);
-    }
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-  public String getLastName() {
-    return lastName;
-  }
-  public int getNumberOfSiblings() {
+ **** Step #2: declare that the Family class is serializable.
+ ***/
+public class Person implements Serializable {
+    // Attributes
+    private String firstName;
+    private String lastName;
     /***
-    **** Step #4: calculate the numOfSiblings attribute
-    **** This should only be done once per execution.
-    ***/
+     **** Step #3: declare that the numOfSiblings attribute is not
+     **** to be serialized.
+     ***/
+    private transient int numOfSiblings;
+    // Associations
+    private Family family = null;
 
-    return numOfSiblings;
-  }
+    // Constructor
+    public Person(Family fam, String f, String l) {
+        firstName = f;
+        lastName = l;
+        family = fam;
+        if (family != null) {
+            family.addChild(this);
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getNumberOfSiblings() {
+        /***
+         **** Step #4: calculate the numOfSiblings attribute
+         **** This should only be done once per execution.
+         ***/
+
+        if (numOfSiblings == 0) {
+            System.out.println("Calculating number of siblings for " + firstName + " " + lastName);
+            numOfSiblings = family.getChildren().size() - 1;
+        }
+
+        return numOfSiblings;
+    }
 }
--- Assignment5/mod15/exercise1/ReadFamilies.java
+++ Assignment5/mod15/exercise1/ReadFamilies.java
@@ -5,35 +5,42 @@ import java.io.ObjectInputStream;
 import java.io.IOException;
 
 public class ReadFamilies {
-  public static void main(String[] args) {
-    Family family1;
-    Family family2;
-    Family family3;
-    Person pat_willcutt;
-    Person bryan_basham;
+    public static void main(String[] args) {
+        Family family1;
+        Family family2;
+        Family family3;
+        Person pat_willcutt;
+        Person bryan_basham;
 
-    try {
-      /***
-      **** Step 8: declare and initialize input stream objects
-      ***/
+        try {
+            FileInputStream fStream = new FileInputStream("families.ser");
+            ObjectInputStream inStream = new ObjectInputStream(fStream);
+            /***
+             **** Step 8: declare and initialize input stream objects
+             ***/
 
-      /***
-      **** Step 9: read the three families from the object stream
-      **** Don't forget to close the top-level stream when done.
-      ***/
+            /***
+             **** Step 9: read the three families from the object stream
+             **** Don't forget to close the top-level stream when done.
+             ***/
 
-      pat_willcutt = family1.findChild("Pat");
-      bryan_basham = family3.findChild("Bryan");
+            family1 = (Family)inStream.readObject();
+            family2 = (Family)inStream.readObject();
+            family3 = (Family)inStream.readObject();
+            inStream.close();
 
-      // queries
-      System.out.println("Pat Willcutt has " + pat_willcutt.getNumberOfSiblings() + " sibilings.");
-      System.out.println("Bryan Basham has " + bryan_basham.getNumberOfSiblings() + " sibilings.");
+            pat_willcutt = family1.findChild("Pat");
+            bryan_basham = family3.findChild("Bryan");
 
-      // Handle exceptions
-    } catch (IOException e1) {
-      e1.printStackTrace();
-    } catch (ClassNotFoundException e2) {
-      e2.printStackTrace();
+            // queries
+            System.out.println("Pat Willcutt has " + pat_willcutt.getNumberOfSiblings() + " sibilings.");
+            System.out.println("Bryan Basham has " + bryan_basham.getNumberOfSiblings() + " sibilings.");
+
+            // Handle exceptions
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (ClassNotFoundException e2) {
+            e2.printStackTrace();
+        }
     }
-  }
 }
